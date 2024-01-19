d71bd3f14497e5c51833e869c174fa132e90133a
haxy <zaidsaheb4@gmail.com>
2024-01-19 13:22:06 +0100

    im so stupid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,7 +127,7 @@ export default function Page() {
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
                       <a className="hover:underline" href={work.link}>
-                        {work.company}
+                        {work.title}
                       </a>
 
                       <span className="inline-flex gap-x-1">
@@ -148,7 +148,7 @@ export default function Page() {
                   </div>
 
                   <h4 className="font-mono text-sm leading-none">
-                    {work.title}
+                    {work.company}
                   </h4>
                 </CardHeader>
                 <CardContent className="mt-2 text-xs">
